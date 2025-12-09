0ac7a81e6d5338d4cc147baea96ceb97fa728348
WanderVandromme <wander.vandromme@student.howest.be>
2025-12-09 17:20:52 +0100

    added different scenes to the horizontal section to redo everything correctly

---
+++ horizontallscroll-test/src/scenes/HorizontalScrollScene/AutoAlignmentScene2.jsx
@@ -0,0 +1,13 @@
+import { Html } from '@react-three/drei'
+
+const AutoAlignmentScene2 = () => {
+    return (
+        <>
+            <div className="horiz_scroll--scene">
+                <h1>Auto Alignment Scene</h1>
+            </div>
+        </>
+    )
+}
+
+export default AutoAlignmentScene2
---
+++ horizontallscroll-test/src/scenes/HorizontalScrollScene/WhyDigiphy.jsx
@@ -0,0 +1,9 @@
+const WhyDigiphy = () => {
+    return (
+        <div className="horiz_scroll--scene">
+            <h1>Why Digiphy Scene</h1>
+        </div>
+    )
+}
+
+export default WhyDigiphy
