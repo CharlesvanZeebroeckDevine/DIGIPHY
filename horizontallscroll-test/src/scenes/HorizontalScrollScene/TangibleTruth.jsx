5d5b69d58ab73c674125d4b1a94355df1f5f2c8d
WanderVandromme <wander.vandromme@student.howest.be>
2025-12-10 10:40:28 +0100

    added DefaultStyles.css with all defaults for font size, spacing and colors

--- horizontallscroll-test/src/scenes/HorizontalScrollScene/TangibleTruth.jsx
+++ horizontallscroll-test/src/scenes/HorizontalScrollScene/TangibleTruth.jsx
@@ -4,8 +4,11 @@ const TangibleTruth = () => {
     return (
         <>
             <div className="horiz_scroll--scene">
-                <h1>Tangible Truth Scene</h1>
                 <img src="/public/assets/tangible-truth-placeholder.png" />
+                <div className="tangible_truth--text">
+                    <h2>The Tangible Truth</h2>
+                    <p>Digital tools rarely give you the confidence of touching the real thing. DigiPHY changes that. Because every movement of the physical proxy is tracked and translated instantly, designers get immediate, tactile validation at every step. Turn it, test it, interact with it, your hands become part of the design process again</p>
+                </div>
             </div>
         </>
     )
