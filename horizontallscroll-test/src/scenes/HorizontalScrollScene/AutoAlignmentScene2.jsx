20190bbf0bd9d5f8b21b85e890b64fd960a088dd
WanderVandromme <wander.vandromme@student.howest.be>
2025-12-10 11:54:54 +0100

    fixed the position of the text on the sidescroll

--- horizontallscroll-test/src/scenes/HorizontalScrollScene/AutoAlignmentScene2.jsx
+++ horizontallscroll-test/src/scenes/HorizontalScrollScene/AutoAlignmentScene2.jsx
@@ -3,7 +3,7 @@ import { Html } from '@react-three/drei'
 const AutoAlignmentScene2 = () => {
     return (
         <>
-            <div className="horiz_scroll--scene horiz_scroll--auto_alignment">
+            <div className="horiz_scroll--scene auto_alignment--container">
                 <div className="auto_alignment--title">
                     <h2>Auto</h2>
                     <h2>Alignment</h2>
@@ -11,7 +11,7 @@ const AutoAlignmentScene2 = () => {
                 <div className="auto_alignment--text">
                     <p>Accuracy is a metric measured in millimeters. DigiPHY uses 10 precision tracking cameras and proprietary algorithms to fuse the physical and virtual worlds instantly, keeping models perfectly aligned no matter how you move.</p>
                 </div>
-                <div>
+                <div className="auto_alignment--text--2">
                     <p>Instant physical feedback makes ideas easier to evaluate, mistakes easier to catch, and decisions easier to make.</p>
                 </div>
             </div>
